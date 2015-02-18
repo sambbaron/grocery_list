5dbef245311595e77080aeb58971289e8d8cf8b5
Sam Baron <sambaron@ownmail.net>
2015-02-17 20:17:02 -0800

    Add reset database script for model development

--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ from flask.ext.script import Manager
 
 from grocery_list import app
 
+from grocery_list.database import Base, engine
+from grocery_list.models import *
+
 manager = Manager(app)
 
 @manager.command
@@ -15,6 +18,14 @@ def run():
     port = int(os.environ.get("PORT", 8000))
     app.run(host="127.0.0.1", port=port)
 
+@manager.command
+def resetdb():
+    """ Drop and create development database """
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    print("Tables Added:")
+    for table in engine.table_names():
+        print(table)
 
 if __name__ == "__main__":
     manager.run()
