6c913fc71b15c6d0c910d8ec9ceddd714bdfcc7e
Sam Baron <sambaron@ownmail.net>
2015-02-19 11:12:23 -0800

    Edit secret key to environment variable or random
    Unable to get environment variable recognized after successfully setting
    env variable through PowerShell, Windows GUI, and Git Bash. This is
    a temporary fix for now. Might cause a problem for returning users.

--- grocery_list/config.py
+++ grocery_list/config.py
@@ -6,9 +6,8 @@ class DevelopmentConfig(object):
     """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = "postgresql://sambbaron:1234@localhost:5432/grocery_list"
     DEBUG = True
-    # SECRET_KEY = "1234"
+    SECRET_KEY = os.environ.get("GROCERYLIST_SECRET_KEY", os.urandom(32))
     # SECRET_KEY = os.environ.get("GROCERYLIST_SECRET_KEY", "")
-    SECRET_KEY = os.urandom(32)
 
 class TestingConfig(object):
     """Development Configuration"""
