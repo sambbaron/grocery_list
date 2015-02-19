4568f6187626ba8ce4c8cf58b0928471a7c04813
Sam Baron <sambaron@ownmail.net>
2015-02-19 13:13:10 -0800

    Fix secret key environment variable
    Use os.environ wrapped in try/except because it calls an error immediately. Exception sets random secret key.

--- grocery_list/config.py
+++ grocery_list/config.py
@@ -6,8 +6,11 @@ class DevelopmentConfig(object):
     """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = "postgresql://sambbaron:1234@localhost:5432/grocery_list"
     DEBUG = True
-    SECRET_KEY = os.environ.get("GROCERYLIST_SECRET_KEY", os.urandom(32))
-    # SECRET_KEY = os.environ.get("GROCERYLIST_SECRET_KEY", "")
+    try:
+        SECRET_KEY = os.environ["MY_SECRET_KEY"]
+    except KeyError as e:
+        print("{}: Secret key not set as environment variable. Setting as random.".format(e))
+        SECRET_KEY = os.urandom(32)
 
 class TestingConfig(object):
     """Development Configuration"""
