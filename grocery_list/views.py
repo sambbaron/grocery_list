8571cce150e12ff13f03e9dab9d46ce95a4bbb0c
Sam Baron <sambaron@ownmail.net>
2015-02-20 13:12:27 -0800

    Fix app index routing
    Convert user_id to integer. Change redirects to routes.

--- grocery_list/views.py
+++ grocery_list/views.py
@@ -18,16 +18,16 @@ def index():
     if current_user.is_anonymous() == True:
         return redirect(url_for("login"))
     
-    user_id = current_user.get_id()
+    user_id = int(current_user.get_id())
     # Redirect to Lists if exist
-    if session.query(List).filter("user_id" == user_id).first() is not None:
-        return redirect(url_for("/lists"))
+    if session.query(List.id).filter(User.id == user_id).first() is not None:
+        return redirect(url_for("lists"))
     # Redirect to Routes if exist
-    elif session.query(Route).filter("user_id" == user_id).first() is not None:
-        return redirect(url_for("/routes"))
+    elif session.query(Route.id).filter(User.id == user_id).first() is not None:
+        return redirect(url_for("routes"))
     # Else redirect to Stores
     else:
-        return redirect(url_for("/stores"))
+        return redirect(url_for("stores"))
 
 
 @app.route("/login", methods=["GET", "POST"])
