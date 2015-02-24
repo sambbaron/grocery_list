b9329d90b4807b22e89a1ee5da4618bb2fd04448
Sam Baron <sambaron@ownmail.net>
2015-02-23 19:58:31 -0800

    Multiple revisions to ajax basic functions, but don't plan to use
    Going to try to use all views and standard form submission

--- grocery_list/static/js/app.js
+++ grocery_list/static/js/app.js
@@ -2,6 +2,31 @@
  * Created by Sam Baron on 2/20/2015.
  */
 
+// Send form data using Ajax
+var sendFormAjax = function(form) {
+
+    var ajax = $.ajax({
+        type: form.elements["_method"].value,
+        url: form.action,
+        async: true,
+        data: JSON.stringify($(form).serializeJSON()),
+        dataType: "json",
+        contentType: "application/json",
+        cache: false,
+        processData: false,
+        complete: function (jqXHR, textStatus) {
+
+            // Set response data and pass to form
+            var respData = JSON.parse(jqXHR.responseText);
+            form.reset();
+            form = respData;
+            console.log(respData);
+            // Set response message
+            var respMsg = respData.message;
+            $("#message").text(respMsg);
+        }
+    });
+};
 
 var sendData = function (button) {
 
@@ -12,53 +37,35 @@ var sendData = function (button) {
     // Set form element
     var form = button.parentNode;
 
-    // Make AJAX request using form method
-    var ajax = $.ajax({
-        type: form.elements["_method"].value,
-        url: form.action,
-        async: true,
-        data: JSON.stringify($(form).serializeJSON()),
-        dataType: "json",
-        contentType: "application/json",
-        cache: false,
-        processData: false,
-        complete: function(jqXHR, textStatus) {
+    // Send form data via Ajax
+    sendFormAjax(form);
 
-            // Reset button
-            button.disabled = "";
-            button.innerHTML = "Submit";
+    // Enable button
+    button.disabled = "";
+    button.innerHTML = "Submit";
 
-            // Set response data and pass to form
-            var respData = jqXHR.responseText;
-            form = respData;
-
-            // Set response message
-            var respMsg = JSON.parse(respData).message;
-            $("#message").text(respMsg);
-
-        }
-    });
-    
 };
 
-//var sendData = function (formID) {
-//    var form = document.getElementById(formID);
-//   // Create a FormData object from the form
-//   // var data = new FormData(form);
-//    var data = $("#"+formID).serializeJSON();
-//    // Make a POST request to the file upload endpoint
-//    var ajax = $.ajax(form.action, {
-//        async: true,
-//        type: 'PUT',
-//        data: JSON.stringify(data),
-//        cache: false,
-//        contentType: "application/json",
-//        processData: false,
-//        dataType: 'json',
-//        complete: function(jqXHR, textStatus) {
-//            $("#message").text(textStatus);
-//
-//            }
-//    });
-//
-//};
+var sendTable = function (button) {
+
+    // Disable button
+    button.disabled = "disabled";
+    button.innerHTML = "Sending";
+
+    // Set table element
+    var table = button.parentNode;
+
+    // Loop through table forms (rows)
+    var forms = table.querySelectorAll("form");
+    Array.prototype.forEach.call(forms, function(form, i) {
+
+        // Send form data via Ajax
+        sendFormAjax(form);
+
+    });
+
+    // Enable button
+    button.disabled = "";
+    button.innerHTML = "Update";
+};
+
