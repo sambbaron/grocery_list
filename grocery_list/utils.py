e6a18fdb96583de8d54452982acb5ca8d1755341
Sam Baron <sambaron@ownmail.net>
2015-02-25 12:48:48 -0800

    Add post new data using function
    Add dictionary parameter with new record primary keys.
    Change primary key separator to space.

--- grocery_list/utils.py
+++ grocery_list/utils.py
@@ -1,20 +1,25 @@
 """ Custom Utility Functions """
 
-from . import app
 from .database import session
 from . import models
 
 
-def update_form_data(data):
-    """ Update SQLAlchemy model data with HTML form data
+def update_form_data(data, **new_primary_keys):
+    """ Create/Update SQLAlchemy model data with HTML form data
+
+    If primary_key not provided in dictionary key, then assume
+        new record and use new_primary_keys object
 
     Arguments:
         data (dict): Dictionary of html form data
             key: Form input name in schema ModelClass.ID.ColumnName
-                ModelClass: SQLAlchemy model class defined in models.py
+                ModelClass: SQLAlchemy model class name defined in models.py
                 ID: Primary Key ID, multiple keys comma separated
                 ColumnName: Column name
             value: Form input value
+        new_primary_keys (dict): Dictionary of new primary keys for post
+            key: Model class name
+            value: Primary key
 
     Return:
         Boolean indicating success
@@ -25,8 +30,14 @@ def update_form_data(data):
 
         # Set key parameters per Form input name schema
         param = str(key).split(".", 2)
+        # SQLAlchemy model table name
         key_model = param[0]
-        key_primary_key = tuple(int(i) for i in param[1].split(","))
+        # Primary Key - return from new_primary_keys if not provided
+        if not param[1] or param[1].isspace():
+            key_primary_key = new_primary_keys[key_model]
+        else:
+            key_primary_key = tuple(int(i) for i in param[1].split(" "))
+        # Column name
         key_column = param[2]
 
         try:
@@ -34,7 +45,7 @@ def update_form_data(data):
             model = getattr(models, key_model)
             # Set row to update
             row = session.query(model).get(key_primary_key)
-            # Perform column update with dict value
+            # Perform record update with dict value
             setattr(row, key_column, value)
             session.commit()
         except Exception as e:
