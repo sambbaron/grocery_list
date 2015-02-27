793b336a26189219263cfa80af5980a5dd440dfa
Sam Baron <sambaron@ownmail.net>
2015-02-26 19:23:56 -0800

    Change Item Group in List Item to optional
    Allows for Lists without Routes.

--- grocery_list/models.py
+++ grocery_list/models.py
@@ -284,4 +284,4 @@ class ListItem(Base):
 
     list_id = Column(Integer, ForeignKey("list.id"), nullable=False, index=True)
     item_measurement_id = Column(Integer, ForeignKey("item_measurement.id"))
-    item_group_id = Column(Integer, ForeignKey("item_group.id"), nullable=False, index=True)
+    item_group_id = Column(Integer, ForeignKey("item_group.id"), index=True)
