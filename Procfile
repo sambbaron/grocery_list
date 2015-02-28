d9ec1a47035b90f4553c9e4c06a528d3e408e97f
Sam Baron <sambaron@ownmail.net>
2015-02-27 22:49:45 -0800

    Revise config for Heroku, but not sure since foreman not working

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn shopping_list:app
+web: gunicorn shopping_list:app --log-file -
--- shopping_list/config.py
+++ shopping_list/config.py
@@ -3,6 +3,13 @@
 import os
 
 
+class HerokuConfig(object):
+    """Heroku Configuration"""
+    SQLALCHEMY_DATABASE_URI = "postgres://whjpwdpzmdwrhc:4ebb7mC0iWfPVHW5FYBjKGH9uk@ec2-23-23-180-133.compute-1.amazonaws.com:5432/d3p4q8u138oe1m"
+    DEBUG = False
+    SECRET_KEY = os.urandom(32)
+
+
 class DevelopmentConfig(object):
     """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = "postgresql://sambbaron:1234@localhost:5432/shopping_list"
